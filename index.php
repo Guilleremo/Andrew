8d8b2a1af1c9e51ff2c0575263d3673aed0755bc
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-05-08 11:56:14 -0700

    finished milestone 2

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+<?php
+//Configuration for our PHP Server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
 
-CLIENT INFO
+//Make Constants using define.
+define('client_id', '3f99e2faa923415b458ab0c4c4b991e');
+define('client_secret', '70d20d0e81714447a98791fb1d69bef8');
+define('redirectURI', 'http://localhost/Andrew/index.php');
+define('ImageDirectory', 'pics/');
+?>
+
+
+<!-- CLIENT INFO
 CLIENT ID 3f99e2faa923415b458ab0c4c4b991e
 CLIENT SECRET 70d20d0e81714447a98791fb1d69bef8
-WEBSITE URL http://localhost/Andrew/iindex.php
-REDIRECT URL http://localhost/Andrew/iindex.php
+WEBSITE URL http://localhost/Andrew/index.php
+REDIRECT URI http://localhost/Andrew/index.php -->
