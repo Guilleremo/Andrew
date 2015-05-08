060c0bf10dafac12f37147b1797afba5c7aa7f4c
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-05-08 11:37:02 -0700

    finished milestone 1

---
+++ index.php
@@ -0,0 +1,6 @@
+
+CLIENT INFO
+CLIENT ID 3f99e2faa923415b458ab0c4c4b991e
+CLIENT SECRET 70d20d0e81714447a98791fb1d69bef8
+WEBSITE URL http://localhost/Andrew/iindex.php
+REDIRECT URL http://localhost/Andrew/iindex.php
