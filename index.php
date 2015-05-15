1742aaca7c6c5811bcf93c8279f8553706ec8ac3
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-05-15 12:17:50 -0700

    finished milestone 12

--- index.php
+++ index.php
@@ -26,50 +26,54 @@ function connectToInstagram($url){
 }
 //Function to get userID cause userName doesn't allow us to get pictures!
 function getUserID($userName){
-	$url = 'http://api.instagram.com/v1/users/search?q='.$userName.'&client_id'.clientID;
+	$url = 'https://api.instagram.com/v1/users/search?q=\"ayoooodrew\"&client_id='.clientID;
 	$instagramInfo = connectToInstagram($url);
 	$results = json_decode($instagramInfo, true);
 
-	echo $results['data']['0']['id'];
+	return $results['data']['0']['id']; //echoing out userID.
 }
 //Function to print out image onto screen
-function printImages($userID){
-	$url = 'http://api.instagram.com/v1/users'.$userID.'/media/recent?client_id='.clientID.'&count=5';
+function printImages($userID)
+{
+	$url = 'https://api.instagram.com/v1/users/' . $userID . '/media/recent?client_id='.clientID . '&count=5';
 	$instagramInfo = connectToInstagram($url);
 	$results = json_decode($instagramInfo, true);
-	//Parse through the information one by one.
-	foreach ($resultd['data']as $items) {
-		$image_url = $items['images']['low_resolution']['url'];//goinng to go through all of my results and give myself back the URL of those pictures because we want to save it  in the PHP Server.
-		echo '<img src" '.$image_url.'"/><br/>';
-	}
+
+	//Parse through thet information one by one
+	foreach($results['data'] as $items)
+	 {
+	 	$image_url = $items['images']['low_resolution']['url']; //go through all of the results and give back the url of those pictures because we want to save it in the php server.
+	 	echo '<img src=" '. $image_url .' "/><br/>';
+	 }
 }
 
 
-if (isset($_GET['code'])){
-	$code = ($_GET['code']);
-	$url = 'https://api.instagram.com/ouath/access_token';
-	$access_token_settings = array('client_id' => clientID,
-									'client_secret' => clientSecret,
-									'grant_type' => 'authorization_code',
-									'redirect_uri' => redirectURI,
-									'code' => $code
-									);
-//cURL is what we use in PHP, it's a library calls to other API's.
-$curl = curl_init($url); //settings a cURL session and we put in $url because that's where we are getting the data from.
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); //setting the POSTFIELDS to the array setup that we created.
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting it equal to 1 because we are getting strings back.
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //but in live work-production we want to set this to true.
+if (isset($_GET['code'])) {
+	$code = $_GET['code'];
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token_settings =  array('client_id' => clientID,
+								   'client_secret' => clientSecret,
+								   'grant_type' => 'authorization_code',
+								   'redirect_uri' => redirectURI,
+								   'code' => $code);
 
+								  
+//Curl is a library that lets you make HTTP requests($_GET[] or $_POST[]) in PHP.
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $result = curl_exec($curl);
 curl_close($curl);
 
 $results = json_decode($result, true);
-
 $userName = $results['user']['username'];
 
+
 $userID = getUserID($userName);
+
 
 printImages($userID);
 }
