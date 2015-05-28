0620f20fcf35e8b9dc79f0fd05c3d24aa04ec999
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-05-28 12:05:29 -0700

    fixed all my errors.

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,7 @@
 	<form id="logout" action="http://localhost/Andrew/index.php">
     	<input type="submit" value="Logout">
+    	<body id="logout">
+    	<link rel="stylesheet" href="css/main.css">
 	</form>
 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
